c3c720545c913fbc41c493b5c834f2518c0d6126
ryudoy <79362075+Loren844@users.noreply.github.com>
2023-05-09 09:31:06 +0200

    test

--- FISK/src/main/java/com/example/ihm/Lanceur.java
+++ FISK/src/main/java/com/example/ihm/Lanceur.java
@@ -11,7 +11,7 @@ public class Lanceur extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Lanceur.class.getResource("menu_map.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Lanceur.class.getResource("menu_titre.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("FISK");
         stage.setScene(scene);
